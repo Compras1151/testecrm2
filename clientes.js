fa470c01e646e0a56b0e4600add60dafccb6badc
Compras1151 <compras1@artegrill.com>
2026-06-24 15:47:41 -0300

    Redirect unauthenticated users to login page

--- clientes.js
+++ clientes.js
@@ -1,3 +1,8 @@
+const usuarioLogado = localStorage.getItem('usuarioLogado');
+
+if (!usuarioLogado) {
+    window.location.href = 'login.html';
+}
 let clientes = JSON.parse(localStorage.getItem('clientesCadastrados')) || [];
 let excluidos = JSON.parse(localStorage.getItem('leadsExcluidos')) || [];
 
