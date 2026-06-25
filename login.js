51438cff5b0184198f89fb034b3ba00705345601
Compras1151 <compras1@artegrill.com>
2026-06-25 13:27:11 -0300

    Comment out login.js functionality

--- login.js
+++ login.js
@@ -1,70 +1,71 @@
-function mostrarToast(mensagem, tipo = "erro") {
+//function mostrarToast(mensagem, tipo = "erro") {
 
-    const container = document.getElementById('toast-container');
+//    const container = document.getElementById('toast-container');
 
-    const toast = document.createElement('div');
+ //   const toast = document.createElement('div');
 
-    toast.className = `toast ${tipo}`;
-    toast.innerText = mensagem;
+//    toast.className = `toast ${tipo}`;
+//    toast.innerText = mensagem;
 
-    container.appendChild(toast);
+//    container.appendChild(toast);
 
-    setTimeout(() => {
-        toast.remove();
-    }, 3000);
-}
+//    setTimeout(() => {
+//        toast.remove();
+//    }, 3000);
 
-document.addEventListener('DOMContentLoaded', () => {
+// }
 
-    let usuarios = JSON.parse(localStorage.getItem('usuariosCRM'));
+//   document.addEventListener('DOMContentLoaded', () => {
+
+  //  let usuarios = JSON.parse(localStorage.getItem('usuariosCRM'));
 
     // Cria usuário admin caso não exista nenhum
-    if (!usuarios || usuarios.length === 0) {
+  //  if (!usuarios || usuarios.length === 0) {
 
-      usuarios = [];
+     // usuarios = [];
 
-        localStorage.setItem(
-            'usuariosCRM',
-            JSON.stringify(usuarios)
-        );
-    }
+      //  localStorage.setItem(
+     //       'usuariosCRM',
+       //     JSON.stringify(usuarios)
+   //     );
+//    }
 
-    const btnEntrar = document.getElementById('btnEntrar');
+ //   const btnEntrar = document.getElementById('btnEntrar');
 
-    btnEntrar.addEventListener('click', () => {
+ //   btnEntrar.addEventListener('click', () => {
 
-        const nome = document.getElementById('usuario').value.trim();
-        const senha = document.getElementById('senha').value.trim();
+   //     const nome = document.getElementById('usuario').value.trim();
+    //    const senha = document.getElementById('senha').value.trim();
 
-        if (!nome || !senha) {
-            mostrarToast("Preencha usuário e senha!");
-            return;
-        }
+    //    if (!nome || !senha) {
+      //      mostrarToast("Preencha usuário e senha!");
+      //      return;
+   //     }
 
-        const usuario = usuarios.find(u =>
-            u.nome.toLowerCase() === nome.toLowerCase() &&
-            u.senha === senha
-        );
+    //    const usuario = usuarios.find(u =>
+        //    u.nome.toLowerCase() === nome.toLowerCase() &&
+       //     u.senha === senha
+  //      );
 
-        console.log("Digitado:", nome, senha);
-        console.log("Usuários:", usuarios);
-        console.log("Encontrado:", usuario);
+ //       console.log("Digitado:", nome, senha);
+  //      console.log("Usuários:", usuarios);
+  //      console.log("Encontrado:", usuario);
 
-        if (!usuario) {
-            mostrarToast("Usuário ou senha inválidos!");
-            return;
-        }
+    //   if (!usuario) {
+      //      mostrarToast("Usuário ou senha inválidos!");
+   //         return;
+   //     }
 
-        localStorage.setItem(
-            'usuarioLogado',
-            JSON.stringify(usuario)
-        );
+    //    localStorage.setItem(
+    //        'usuarioLogado',
+       //     JSON.stringify(usuario)
+     //   );
 
-        mostrarToast("Login realizado com sucesso!", "sucesso");
+       // mostrarToast("Login realizado com sucesso!", "sucesso");
 
-        setTimeout(() => {
-            window.location.href = 'index.html';
-        }, 1000);
-    });
+    //    setTimeout(() => {
+     //       window.location.href = 'index.html';
+       // }, 1000);
+ //   });
 
-});
+//});
